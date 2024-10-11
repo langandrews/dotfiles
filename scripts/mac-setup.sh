2c8a0db668d7d02397860b8735331055aee54ffe
Andrew Lang <andrew.lang@irrigreen.com>
2024-10-11 16:47:59 -0500

    mac-setup script

---
+++ scripts/mac-setup.sh
@@ -0,0 +1,8 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew tap homebrew/cask-fonts
+brew install font-meslo-lg-nerd-font
+brew install node
+brew install ripgrep
+brew install tree
+brew install neovim
+brew install ghc
